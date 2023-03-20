36fff00df447df239d3e7c11e45f7fab85536e7d
yelsin96 <yelsinsilva@gmail.com>
2023-03-20 10:17:00 -0500

    se ingresa bd y inicios de responsive chat

--- index.php
+++ index.php
@@ -14,86 +14,88 @@ include('header.php');
 		<?php if (isset($_SESSION['userid']) && $_SESSION['userid']) { ?>
 			<div class="chat">
 				<div id="frame">
-					<div id="sidepanel">
-						<div id="profile">
-							<?php
-							include('Chat.php');
-							$chat = new Chat();
-							$loggedUser = $chat->getUserDetails($_SESSION['userid']);
-							echo '<div class="wrap">';
-							$currentSession = '';
-							foreach ($loggedUser as $user) {
-								$currentSession = $user['current_session'];
-								echo '<img id="profile-img" src="data:image/png;base64, '.base64_encode($user['avatar']).' " class="online" alt="" />';
-								echo  '<p class="d-none d-sm-block">' . $user['username'] . '</p>';
-								echo '<i class="fa fa-chevron-down expand-button" aria-hidden="true"></i>';
-								echo '<div id="status-options">';
-								echo '<ul>';
-								echo '<li id="status-online" class="active"><span class="status-circle"></span> <p>Online</p></li>';
-								echo '<li id="status-away"><span class="status-circle"></span> <p>Ausente</p></li>';
-								echo '<li id="status-busy"><span class="status-circle"></span> <p>Ocupado</p></li>';
-								echo '<li id="status-offline"><span class="status-circle"></span> <p>Desconectado</p></li>';
-								echo '</ul>';
+					<div class="row" style="height: 100%;">
+						<div id="sidepanel" class="col-1 col-md-4 col-sm-2">
+							<div id="profile">
+								<?php
+								include('Chat.php');
+								$chat = new Chat();
+								$loggedUser = $chat->getUserDetails($_SESSION['userid']);
+								echo '<div class="wrap">';
+								$currentSession = '';
+								foreach ($loggedUser as $user) {
+									$currentSession = $user['current_session'];
+									echo '<img id="profile-img" src="data:image/png;base64, ' . base64_encode($user['avatar']) . ' " class="online" alt="" />';
+									echo  '<p class="d-none d-sm-block">' . $user['username'] . '</p>';
+									echo '<i class="fa fa-chevron-down expand-button" aria-hidden="true"></i>';
+									echo '<div id="status-options">';
+									echo '<ul>';
+									echo '<li id="status-online" class="active"><span class="status-circle"></span> <p>Online</p></li>';
+									echo '<li id="status-away"><span class="status-circle"></span> <p>Ausente</p></li>';
+									echo '<li id="status-busy"><span class="status-circle"></span> <p>Ocupado</p></li>';
+									echo '<li id="status-offline"><span class="status-circle"></span> <p>Desconectado</p></li>';
+									echo '</ul>';
+									echo '</div>';
+									echo '<div id="expanded">';
+									echo '<a href="logout.php">Salir</a>';
+									echo '</div>';
+								}
 								echo '</div>';
-								echo '<div id="expanded">';
-								echo '<a href="logout.php">Salir</a>';
-								echo '</div>';
-							}
-							echo '</div>';
-							?>
-						</div>
-						<!-- <div id="search">
+								?>
+							</div>
+							<!-- <div id="search">
 							<label for=""><i class="fa fa-search" aria-hidden="true"></i></label>
 							<input type="text" placeholder="Buscar Contactos..." />
 						</div> -->
-						<div id="contacts">
-							<?php
-							echo '<ul>';
-							$chatUsers = $chat->chatUsers($_SESSION['userid']);
-							foreach ($chatUsers as $user) {
-								$status = 'offline';
-								if ($user['online']) {
-									$status = 'online';
+							<div id="contacts">
+								<?php
+								echo '<ul>';
+								$chatUsers = $chat->chatUsers($_SESSION['userid']);
+								foreach ($chatUsers as $user) {
+									$status = 'offline';
+									if ($user['online']) {
+										$status = 'online';
+									}
+									$activeUser = '';
+									if ($user['userid'] == $currentSession) {
+										$activeUser = "active";
+									}
+									echo '<li id="' . $user['userid'] . '" class="contact ' . $activeUser . '" data-touserid="' . $user['userid'] . '" data-tousername="' . $user['username'] . '">';
+									echo '<div class="wrap">';
+									echo '<span id="status_' . $user['userid'] . '" class="contact-status ' . $status . '"></span>';
+									echo '<img src="data:image/png;base64, ' . base64_encode($user['avatar']) . ' " alt="" />';
+									echo '<div class="meta">';
+									echo '<p class="name d-none d-sm-block">' . $user['username'] . '<span id="unread_' . $user['userid'] . '" class="unread">' . $chat->getUnreadMessageCount($user['userid'], $_SESSION['userid']) . '</span></p>';
+									echo '<p class="preview"><span id="isTyping_' . $user['userid'] . '" class="isTyping"></span></p>';
+									echo '</div>';
+									echo '</div>';
+									echo '</li>';
 								}
-								$activeUser = '';
-								if ($user['userid'] == $currentSession) {
-									$activeUser = "active";
+								echo '</ul>';
+								?>
+							</div>
+						</div>
+						<div class="content col-11 col-md-8 col-sm-10 " id="content">
+							<div class="contact-profile" id="userSection">
+								<?php
+								$userDetails = $chat->getUserDetails($currentSession);
+								foreach ($userDetails as $user) {
+									echo '<img src="data:image/png;base64, ' . base64_encode($user['avatar']) . ' " alt="" />';
+									echo '<p>' . $user['username'] . '</p>';
 								}
-								echo '<li id="' . $user['userid'] . '" class="contact ' . $activeUser . '" data-touserid="' . $user['userid'] . '" data-tousername="' . $user['username'] . '">';
-								echo '<div class="wrap">';
-								echo '<span id="status_' . $user['userid'] . '" class="contact-status ' . $status . '"></span>';
-								echo '<img src="data:image/png;base64, '.base64_encode($user['avatar']).' " alt="" />';
-								echo '<div class="meta">';
-								echo '<p class="name d-none d-sm-block">' . $user['username'] . '<span id="unread_' . $user['userid'] . '" class="unread">' . $chat->getUnreadMessageCount($user['userid'], $_SESSION['userid']) . '</span></p>';
-								echo '<p class="preview"><span id="isTyping_' . $user['userid'] . '" class="isTyping"></span></p>';
-								echo '</div>';
-								echo '</div>';
-								echo '</li>';
-							}
-							echo '</ul>';
-							?>
-						</div>
-					</div>
-					<div class="content" id="content">
-						<div class="contact-profile" id="userSection">
-							<?php
-							$userDetails = $chat->getUserDetails($currentSession);
-							foreach ($userDetails as $user) {
-								echo '<img src="data:image/png;base64, '.base64_encode($user['avatar']).' " alt="" />';
-								echo '<p>' . $user['username'] . '</p>';
-							}
-							?>
-						</div>
-						<div class="messages" id="conversation">
-							<?php
-							echo $chat->getUserChat($_SESSION['userid'], $currentSession);
-							?>
-						</div>
-						<div class="message-input" id="replySection">
-							<div class="message-input" id="replyContainer">
-								<div class="wrap">
-									<input type="text" class="chatMessage" id="chatMessage<?php echo $currentSession; ?>" placeholder="Escribe tu mensaje..." />
-									<button class="submit chatButton" id="chatButton<?php echo $currentSession; ?>"><i class="fa fa-paper-plane" aria-hidden="true"></i></button>
+								?>
+							</div>
+							<div class="messages" id="conversation">
+								<?php
+								echo $chat->getUserChat($_SESSION['userid'], $currentSession);
+								?>
+							</div>
+							<div class="message-input" id="replySection">
+								<div class="message-input" id="replyContainer">
+									<div class="wrap">
+										<input type="text" class="chatMessage" id="chatMessage<?php echo $currentSession; ?>" placeholder="Escribe tu mensaje..." />
+										<button class="submit chatButton" id="chatButton<?php echo $currentSession; ?>"><i class="fa fa-paper-plane" aria-hidden="true"></i></button>
+									</div>
 								</div>
 							</div>
 						</div>
